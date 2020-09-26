1791a20cb5f57abe9c2a73ea1a2c1ab2a0cbe8d3
Maria Quinton <maria.quinton28@gmail.com>
2020-09-25 22:15:56 -0400

    Starting files, created test script to run my data

---
+++ PyBank/analysis/testScript.py
@@ -0,0 +1,17 @@
+# Modules
+import os
+import csv
+
+# set path for file
+csvpath = os.path.join("..", "Resources", "02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv")
+
+# Variables
+
+# Open the CSV
+def budget_data(filepath):
+    with open(filepath, "r") as profit:
+        csvreader = csv.reader(csvpath, delimiter=",")
+
+    header = next(csvreader)
+
+    for months in range(profit):
