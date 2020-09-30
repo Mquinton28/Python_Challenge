771d78697168d812eac12c92afcda5fc50fc2a03
Maria Quinton <maria.quinton28@gmail.com>
2020-09-30 08:00:34 -0500

    Finished PyBank, working out kinks with PyPoll

--- PyPoll/analysis/testDataPyPoll.py
+++ PyPoll/analysis/testDataPyPoll.py
@@ -3,4 +3,23 @@ import os
 import csv
 
 # set path for file
-csvpath = os.path.join("..", "Resources", "02-Homework_03-Python_Instructions_PyBank_Resources_election_data.csv")
+election_data = os.path.join("..", "Resources", "02-Homework_03-Python_Instructions_PyBank_Resources_election_data.csv")
+
+# create lists
+total_votes = []
+candidates = []
+percentage_of_votes = []
+
+with open(election_data) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
+    for row in csvreader:
+        total_votes.append(row[0])
+        candidates.appeend(str(row[2])
+
+max = 0
+min = 0
+max_index = 0
+min_index = 0
+
+#Print Statemenets
