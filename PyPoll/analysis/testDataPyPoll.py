4e436242f54a858850eb12dde31abb5390bb3853
Maria Quinton <maria.quinton28@gmail.com>
2020-09-30 09:20:43 -0500

    Working through PyPoll kinks

--- PyPoll/analysis/testDataPyPoll.py
+++ PyPoll/analysis/testDataPyPoll.py
@@ -8,18 +8,28 @@ election_data = os.path.join("..", "Resources", "02-Homework_03-Python_Instructi
 # create lists
 total_votes = []
 candidates = []
+results = []
 percentage_of_votes = []
 
+# open csv
 with open(election_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
     for row in csvreader:
         total_votes.append(row[0])
-        candidates.appeend(str(row[2])
-
-        print("Election Results")
+        candidates.append(str(row[2])
 
 # max = 0
 # min = 0
 # max_index = 0
 # min_index = 0
+
+# percent = round(int(total_votes) / len(total_votes), 2)
+# percentage_of_votes.append(percent)
+# Print Statements
+#print("Election Results")
+#print("----------------------")
+#print(f"Total Votes: {len(total_votes)}")
+#print("------------------------")
+
+# output file
